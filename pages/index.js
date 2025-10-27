53c7a6ac019146266d902663fd8867c59f9f2a6e
valesc4 <valescacarneiro1115@gmail.com>
2025-10-27 10:57:40 +0000

    atualização da página inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>Welcome</h1>
+    return <h1>Welcome to the Project</h1>
 }
 
 export default Home; // export default define a função Home como padrão que vai ser chamada primeiro
