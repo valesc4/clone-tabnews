2e91f4e1559fdee457ca493de4328a7278411b18
valesc4 <valescacarneiro1115@gmail.com>
2025-11-20 14:50:43 +0000

    welcome update

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-  return <h1>Welcome to the Project</h1>;
+  return (
+    <h1>
+      Welcome! Esse espaço será dedicado para compartilhar dicas e conteúdos de
+      cybersecurity, majortariamente em português. A intenção é ajudar a
+      comunidade brasileira de estudantes de cyber.
+    </h1>
+  );
 }
 
 export default Home; // export default define a função Home como padrão que vai ser chamada primeiro
