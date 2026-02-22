c71789ead90db8705a955dd03de756765f43ed67
valesc4 <valescacarneiro1115@gmail.com>
2026-02-22 17:04:53 +0000

    update database

--- infra/database.js
+++ infra/database.js
@@ -3,9 +3,9 @@ import { Client } from "pg";
 async function query(queryObject) {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRESS_PORT,
-    user: process.env.POSTGRESS_USER,
-    database: process.env.POSTGRESS_DATABASE,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
   await client.connect();
