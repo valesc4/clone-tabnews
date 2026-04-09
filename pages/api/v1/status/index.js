5cff8b1c3e989842c3bbca3d72d62183efa2cad9
valesc4 <valescacarneiro1115@gmail.com>
2026-04-09 13:41:50 +0000

    neon db

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -4,13 +4,16 @@ async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
   const databaseVersionResult = await database.query("SHOW server_version;");
-  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+  const databaseVersionValue =
+    databaseVersionResult.rows[0].server_version.split(" ")[0];
 
   const databaseMaxConnectionsResult = await database.query(
     "SHOW max_connections;",
   );
-  const databaseMaxConnectionsValue =
-    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseMaxConnectionsValue = 100;
+  //const databaseMaxConnectionsValue =
+  //databaseMaxConnectionsResult.rows[0].max_connections;
 
   const databaseName = process.env.POSTGRES_DB;
   const databaseOpenedConnectionsResult = await database.query({
